2f0f2984acdad6e72f3803ab45987f71c5e8589a
Rob Bygrave <robin.bygrave@gmail.com>
2022-12-16 00:45:13 +1300

    Add headers to the test

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -6,7 +6,9 @@ import io.helidon.nima.webserver.WebServer;
 import io.helidon.nima.webserver.http.HttpRouting;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.io.OutputStream;
+import java.io.UncheckedIOException;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 
@@ -33,17 +35,26 @@ class MainTest {
       final var builder = HttpRouting.builder()
         .get("good", (req, res) -> {
           res.status(Http.Status.OK_200);
+          res.header("main-handler-header", "a");
           res.send("good".getBytes(StandardCharsets.UTF_8));
         })
         .get("bad", (req, res) -> {
           res.status(Http.Status.OK_200);
+          res.header("main-handler-header", "a");
           OutputStream outputStream = res.outputStream();
           // error occurs before writing content to outputStream
           throw new RuntimeException("Error After res.outputStream() called");
         })
         .error(Exception.class, (req, res, e) -> {
           res.status(Http.Status.I_AM_A_TEAPOT_418);
-          res.send("MyErrorMessage".getBytes(StandardCharsets.UTF_8));
+          res.header("error-handler-header", "b");
+          OutputStream os = res.outputStream();
+          try {
+            os.write("MyErrorMessage".getBytes(StandardCharsets.UTF_8));
+            os.close();
+          } catch (IOException ex) {
+            throw new UncheckedIOException(ex);
+          }
         });
 
       webServer = WebServer.builder()
@@ -73,6 +84,8 @@ class MainTest {
 
       HttpResponse<String> goodResponse = httpClient.request().path("good").GET().asString();
       assertThat(goodResponse.statusCode()).isEqualTo(200);
+      assertThat(goodResponse.headers().firstValue("main-handler-header")).isPresent().get().isEqualTo("a");
+
       assertThat(goodResponse.body()).isEqualTo("good");
     } finally {
       myServer.stop();
@@ -90,6 +103,9 @@ class MainTest {
       // we hope to get our error handler response, but we don't get it due to internal error
       assertThat(badResponse.statusCode()).isEqualTo(418); // Fail: Get 500
       assertThat(badResponse.body()).isEqualTo("MyErrorMessage");
+      assertThat(badResponse.headers().firstValue("main-handler-header")).isEmpty();
+      assertThat(badResponse.headers().firstValue("error-handler-header")).isPresent().get().isEqualTo("b");
+
     } finally {
       myServer.stop();
     }
